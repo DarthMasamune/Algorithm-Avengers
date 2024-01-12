6107b660904920c613641a4fc4e8620a24a4b57a
mc4lucas <lucasross1234@gmail.com>
2024-01-12 17:59:15 -0500

    add

--- FileReader.js
+++ FileReader.js
@@ -1,2 +1,10 @@
 //This scripts purpose is to read the csv file / database.
 
+const reader = new FileReader(); //The reader gains access to the csv file
+const submit = document.getElementById("submitBTN").addEventListener("click", updateFile); //When submit button is clicked it will run updateFile()
+
+function updateFile(){
+    const file = document.getElementById("myFile"); //Route access to file
+    reader.readAsText(file);
+}
+
